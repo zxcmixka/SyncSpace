d4f4efd57c937cae95fecd70d3c42b6b21f27039
zxcmixka <pikopavel@gmail.com>
2025-11-07 05:13:03 +0300

    Start client dev

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+      <div class="w-full h-full bg-black py-0 my-0">
+        <div class=" py-10 my-5 bg-neutral-800 border-e-neutral-900">
+          <h1>123</h1>
+        </div>
+      </div>
+  )
+}
+
+export default App
