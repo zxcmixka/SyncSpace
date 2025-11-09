447484cf7ec546021c60c67d37aab0298ed47c3a
zxcmixka <pikopavel@gmail.com>
2025-11-09 08:58:29 +0300

    start create page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
+import {Routes, Route } from "react-router-dom";
+import Fail from './page/404/404.tsx'
+import Home from './page/home/Home.tsx'
 
 function App() {
 
 
   return (
-      <div className="bg">
-        <div className="main"></div>
-      </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="*" element={<Fail/>}/>
+      </Routes>
+    </>    
   )
 }
 
